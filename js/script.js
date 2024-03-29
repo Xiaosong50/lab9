28b225ec39daea326d9d7c9d9a13b9a5de467614
Xiaosong50 <xlu50@hawk.iit.edu>
2024-03-28 19:56:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
+  var button = document.getElementById('entrybutton');
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Xiaosong Lu: " + textbox.value);
+  }
+  button.addEventListener('click', showMeText);
+}
 window.addEventListener('load', init);
